71d3ad74b60bcc2f8330e818e12d8a870eb36d56
Khalid Hassan <101973900+KhalidHassan218@users.noreply.github.com>
2023-08-14 02:43:13 +0400

    Update sendEmail.js

--- Utils/sendEmail.js
+++ Utils/sendEmail.js
@@ -3,7 +3,7 @@ const nodemailer = require("nodemailer");
 const sendEmail = async (subject, message, send_to, sent_from, reply_to) => {
   const transporter = nodemailer.createTransport({
     // host: process.env.EMAIL_HOST,
-    name:'smtp.office365.com'T,
+    name:'smtp.office365.com',
     host: 'smtp.office365.com',
   port: 587,
   secure: false, // true for 465, false for other ports
